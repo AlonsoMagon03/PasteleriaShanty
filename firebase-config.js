735ece36857beb64fd9cda56f4850887c91a7fc8
AlonsoMagon03 <146474316+AlonsoMagon03@users.noreply.github.com>
2025-04-24 23:39:26 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // firebase-config.js
 const firebaseConfig = {
-    apiKey: "AIzaSyDbthH3gkv65-oec61OiQ1liltd5neWuRc",
-    authDomain: "encuestashanty.firebaseapp.com",
-    projectId: "encuestashanty",
-    storageBucket: "encuestashanty.firebasestorage.app",
-    messagingSenderId: "830229743734",
-    appId: "1:830229743734:web:19e220f264303f4a62b757",
-    measurementId: "G-NVSNS7PG6R"
-  };
+  apiKey: "AIzaSyDbthH3gkv65-oec61OiQ1liltd5neWuRc",
+  authDomain: "encuestashanty.firebaseapp.com",
+  projectId: "encuestashanty",
+  storageBucket: "encuestashanty.firebasestorage.app",
+  messagingSenderId: "830229743734",
+  appId: "1:830229743734:web:19e220f264303f4a62b757",
+  measurementId: "G-NVSNS7PG6R"
+}
 // Inicializa Firebase
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
